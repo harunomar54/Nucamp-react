099413e2db9b6d22809c535d36cb13a4a6f6c778
harunomar54 <harun.o.g54@gmail.com>
2023-02-10 15:06:02 -0500

    added CSS file and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello NuCamp</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Ready To Work
+    </div>
+  );
 }
 
 export default App;
