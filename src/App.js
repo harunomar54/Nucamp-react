f09c822f18d3cad4ae8963bf214d0484f0865ab5
harunomar54 <harun.o.g54@gmail.com>
2023-02-11 16:39:45 -0500

    Added Nav and Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import NucampLogo from "./app/assets/img/logo.png";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="">
-            <img src={NucampLogo} alt="nucamp logo" />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      Ready To Work
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
