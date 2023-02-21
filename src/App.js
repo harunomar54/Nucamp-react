6b45af0985ced562bd28a5afd2a0e0e9c4997a2a
harunomar54 <harun.o.g54@gmail.com>
2023-02-21 18:25:45 -0500

    several additions to features and components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import { Routes, Route } from "react-router-dom";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 import "./App.css";
-import CampsitesList from "./features/campsites/CampsitesList";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
       <Footer />
     </div>
   );
